0039a3cacebcfbd06bcfaec9982452569757c3d0
Yefry-yeff <55094597+Yefry-yeff@users.noreply.github.com>
2021-03-17 16:06:36 -0600

    email

--- SISTEMA/HUMANSYS/resources/views/auth/forgot-password.blade.php
+++ SISTEMA/HUMANSYS/resources/views/auth/forgot-password.blade.php
@@ -26,7 +26,7 @@
 
             <div class="flex items-center justify-end mt-4">
                 <x-jet-button>
-                    {{ __('Email Password Reset Link') }}
+                    {{ __('Presione para reestablecer') }}
                 </x-jet-button>
             </div>
         </form>
