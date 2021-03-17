c7e4fe60d48e2e018156c1f5446c49e143b717fc
ariel112 <selvinmorazan@gmail.com>
2021-03-17 09:35:08 -0600

    navmenu

--- SISTEMA/HUMANSYS/resources/views/navigation-menu.blade.php
+++ SISTEMA/HUMANSYS/resources/views/navigation-menu.blade.php
@@ -31,41 +31,7 @@
 				<!-- Header Menu -->
 				<ul class="nav user-menu">
 				
-					<!-- Search -->
-					{{-- <li class="nav-item">
-						<div class="top-nav-search">
-							<a href="javascript:void(0);" class="responsive-search">
-								<i class="fa fa-search"></i>
-						   </a>
-							<form action="search.html">
-								<input class="form-control" type="text" placeholder="Search here">
-								<button class="btn" type="submit"><i class="fa fa-search"></i></button>
-							</form>
-						</div>
-					</li> --}}
-					<!-- /Search -->
 				
-					<!-- Flag -->
-					{{-- <li class="nav-item dropdown has-arrow flag-nav">
-						<a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button">
-							<img src="assets/img/flags/us.png" alt="" height="20"> <span>English</span>
-						</a>
-						<div class="dropdown-menu dropdown-menu-right">
-							<a href="javascript:void(0);" class="dropdown-item">
-								<img src="assets/img/flags/us.png" alt="" height="16"> English
-							</a>
-							<a href="javascript:void(0);" class="dropdown-item">
-								<img src="assets/img/flags/fr.png" alt="" height="16"> French
-							</a>
-							<a href="javascript:void(0);" class="dropdown-item">
-								<img src="assets/img/flags/es.png" alt="" height="16"> Spanish
-							</a>
-							<a href="javascript:void(0);" class="dropdown-item">
-								<img src="assets/img/flags/de.png" alt="" height="16"> German
-							</a>
-						</div>
-					</li> --}}
-					<!-- /Flag -->
 				
 					<!-- Notifications -->
 					<li class="nav-item dropdown">
@@ -153,110 +119,7 @@
 					</li>
 					<!-- /Notifications -->
 					
-					<!-- Message Notifications -->
-					<li class="nav-item dropdown">
-						<a href="#" class="dropdown-toggle nav-link" data-toggle="dropdown">
-							<i class="fa fa-comment-o"></i> <span class="badge badge-pill">8</span>
-						</a>
-						<div class="dropdown-menu notifications">
-							<div class="topnav-dropdown-header">
-								<span class="notification-title">Messages</span>
-								<a href="javascript:void(0)" class="clear-noti"> Clear All </a>
-							</div>
-							<div class="noti-content">
-								<ul class="notification-list">
-									<li class="notification-message">
-										<a href="chat.html">
-											<div class="list-item">
-												<div class="list-left">
-													<span class="avatar">
-														<img alt="" src="assets/img/profiles/avatar-09.jpg">
-													</span>
-												</div>
-												<div class="list-body">
-													<span class="message-author">Richard Miles </span>
-													<span class="message-time">12:28 AM</span>
-													<div class="clearfix"></div>
-													<span class="message-content">Lorem ipsum dolor sit amet, consectetur adipiscing</span>
-												</div>
-											</div>
-										</a>
-									</li>
-									<li class="notification-message">
-										<a href="chat.html">
-											<div class="list-item">
-												<div class="list-left">
-													<span class="avatar">
-														<img alt="" src="assets/img/profiles/avatar-02.jpg">
-													</span>
-												</div>
-												<div class="list-body">
-													<span class="message-author">John Doe</span>
-													<span class="message-time">6 Mar</span>
-													<div class="clearfix"></div>
-													<span class="message-content">Lorem ipsum dolor sit amet, consectetur adipiscing</span>
-												</div>
-											</div>
-										</a>
-									</li>
-									<li class="notification-message">
-										<a href="chat.html">
-											<div class="list-item">
-												<div class="list-left">
-													<span class="avatar">
-														<img alt="" src="assets/img/profiles/avatar-03.jpg">
-													</span>
-												</div>
-												<div class="list-body">
-													<span class="message-author"> Tarah Shropshire </span>
-													<span class="message-time">5 Mar</span>
-													<div class="clearfix"></div>
-													<span class="message-content">Lorem ipsum dolor sit amet, consectetur adipiscing</span>
-												</div>
-											</div>
-										</a>
-									</li>
-									<li class="notification-message">
-										<a href="chat.html">
-											<div class="list-item">
-												<div class="list-left">
-													<span class="avatar">
-														<img alt="" src="assets/img/profiles/avatar-05.jpg">
-													</span>
-												</div>
-												<div class="list-body">
-													<span class="message-author">Mike Litorus</span>
-													<span class="message-time">3 Mar</span>
-													<div class="clearfix"></div>
-													<span class="message-content">Lorem ipsum dolor sit amet, consectetur adipiscing</span>
-												</div>
-											</div>
-										</a>
-									</li>
-									<li class="notification-message">
-										<a href="chat.html">
-											<div class="list-item">
-												<div class="list-left">
-													<span class="avatar">
-														<img alt="" src="assets/img/profiles/avatar-08.jpg">
-													</span>
-												</div>
-												<div class="list-body">
-													<span class="message-author"> Catherine Manseau </span>
-													<span class="message-time">27 Feb</span>
-													<div class="clearfix"></div>
-													<span class="message-content">Lorem ipsum dolor sit amet, consectetur adipiscing</span>
-												</div>
-											</div>
-										</a>
-									</li>
-								</ul>
-							</div>
-							<div class="topnav-dropdown-footer">
-								<a href="chat.html">View all Messages</a>
-							</div>
-						</div>
-					</li>
+				
 					<!-- /Message Notifications -->
 
 					<li class="nav-item dropdown has-arrow main-drop">
@@ -264,7 +127,7 @@
 
                             @if (Laravel\Jetstream\Jetstream::managesProfilePhotos())
                                
-                            <span class="user-img"> 
+                            <span class="user-img mt-3"> 
                                 <img  src="{{ Auth::user()->profile_photo_url }}" alt="{{ Auth::user()->name }}" />
                             <span class="status online"></span>
                             </span>
@@ -272,7 +135,8 @@
                             @endif
 						
 
-							<span class="status online"></span></span>
+							{{-- <span class="status online"></span> --}}
+							</span>
 							<span>{{ Auth::user()->name }}</span>
 						</a>
 						<div class="dropdown-menu">
@@ -308,7 +172,24 @@
 				
 				<!-- Mobile Menu -->
 				<div class="dropdown mobile-user-menu">
-					<a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown" aria-expanded="false"><i class="fa fa-ellipsis-v"></i></a>
+					<a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown" aria-expanded="false">
+					  	
+
+                            @if (Laravel\Jetstream\Jetstream::managesProfilePhotos())
+                               
+                            <span class="user-img" style="width:30px; heigth:35px; margin-top:12px;"> 
+                                <img  src="{{ Auth::user()->profile_photo_url }}" style="border-radius:60%;"  />
+                            <span class="status online"></span>
+                            </span>
+                                
+                            @endif
+						
+
+							{{-- <span class="status online"></span> --}}
+							</span>
+							
+						
+					</a>
 					<div class="dropdown-menu dropdown-menu-right">
 						<a class="dropdown-item" >
                             <x-jet-responsive-nav-link href="{{ route('profile.show') }}" :active="request()->routeIs('profile.show')">
