77a4143ce220c29277c5f07805a5545dd6bcc017
cleeper20 <39780147+cleeper20@users.noreply.github.com>
2021-03-22 16:00:58 -0600

    permisos

--- SISTEMA/HUMANSYS/public/assets/js/permisos/permisos.js
+++ SISTEMA/HUMANSYS/public/assets/js/permisos/permisos.js
@@ -11,7 +11,7 @@ function opciones(){
 
  axios.get('/listado/permisos')
  .then( response=>{
-     console.log(response.data)
+     //console.log(response.data)
 
      let lista ="";
      response.data.tipos.forEach(element => {
@@ -30,6 +30,55 @@ function opciones(){
   
 })  
 
+
+
+}
+
+
+function enviarPermiso(){
+
+       let y = document.getElementById('fechaInicio').value;
+       let ddi = y.substring(0, 2);
+       let mmi = y.substring(3,5);
+       let yyi = y.substring(6,10);
+
+       let fechaInicio = yyi+'-'+mmi+'-'+ddi;
+
+    
+       let x=   document.getElementById('fechaFinal').value;
+       let ddf = x.substring(0, 2);
+       let mmf = x.substring(3,5);
+       let yyf = x.substring(6,10);
+
+       let fechaFinal = yyf+'-'+mmf+'-'+ddf;
+
+       if(fechaFinal == fechaInicio){
+        document.getElementById("horasPermisos").className = "d-block";
+       }else{
+        document.getElementById("horasPermisos").className = "d-none";
+       }
+       
+       let horaInicio = document.getElementById('horaInicial').value;
+       let horaFinal = document.getElementById('horaFinal').value;
+
+
+       if(fechaFinal == fechaInicio){
+
+            if(horaInicio && horaFinal){
+                    //guardar datos con hora
+            }
+
+       }else{
+         //guardar fechas
+       }
+
+
+       
+       
+       console.log(horaInicio,horaFinal);
+
+
+       return;
 
 
 }
