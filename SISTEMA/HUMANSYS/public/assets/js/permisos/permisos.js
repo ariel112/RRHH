236e00b0705eda5d2840b54c8ab948eddc0bf8ce
cleeper20 <39780147+cleeper20@users.noreply.github.com>
2021-03-22 13:39:50 -0600

    modal permiso

--- SISTEMA/HUMANSYS/public/assets/js/permisos/permisos.js
+++ SISTEMA/HUMANSYS/public/assets/js/permisos/permisos.js
@@ -1,22 +1,34 @@
-
-
 $(document).ready(opciones);
 
 function opciones(){
    
 
- var opciones = document.getElementById('permisosEmpleado').innerHTML=`
- <option>Casual Leave 12 Days</option>
- <option>Prueba de inner</option>
+//  var opciones = document.getElementById('permisosEmpleado').innerHTML=`
+//  <option>Casual Leave 12 Days</option>
+//  <option>Prueba de inner</option>
  
- `
+//  `
 
  axios.get('/listado/permisos')
  .then( response=>{
      console.log(response.data)
- } )
 
-    
+     let lista ="";
+     response.data.tipos.forEach(element => {
+
+        lista = lista + `<option value="${element.id}">${element.permiso}</option>`
+         
+     });
+
+     document.getElementById('permisosEmpleado').innerHTML= lista;
+
+
+ })
+ .catch( err =>{
+    // console.error(err,"entro");
+  console.error(err.response.data.exception)
+  
+})  
 
 
 
