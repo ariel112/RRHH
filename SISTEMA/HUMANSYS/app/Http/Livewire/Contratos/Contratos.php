66f388e848ec0021cfbac4544f7e0fa260b1643f
ariel112 <selvinmorazan@gmail.com>
2021-03-22 16:32:22 -0600

    CONTRATOS

--- SISTEMA/HUMANSYS/app/Http/Livewire/Contratos/Contratos.php
+++ SISTEMA/HUMANSYS/app/Http/Livewire/Contratos/Contratos.php
@@ -4,6 +4,7 @@ namespace App\Http\Livewire\Contratos;
 
 use Livewire\Component;
 use Illuminate\Http\Request;
+use App\Models\contrato;
 
 class Contratos extends Component
 {
@@ -14,6 +15,21 @@ class Contratos extends Component
 
     public function contrato_show(Request $request){
 
+        $contrato = new contrato();
+        $contrato->num_contrato = $request->num_contrato;
+        $contrato->tipo_contrato = $request->tipo_contrato;
+        $contrato->fecha_inicio = $request->fecha_inicio;
+        $contrato->fecha_fin = $request->fecha_fin;
+        $contrato->sueldo = $request->sueldo;
+        $contrato->vacaciones = $request->vacaciones;
+        $contrato->empleado_id= $request->empleado_id;
+        $contrato->horarios_id = $request->horarios_id;
+        $contrato->users_aprueba_id = $request->users_aprueba_id;
+        $contrato->empleado_rrhh = $request->empleado_rrhh;
+
+        $contrato->save();
+
+        return 'EXITO';
 
 
     }
