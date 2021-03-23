83df3536c0cb2380d54414e1031f27b8e9cee94d
Yefry-yeff <55094597+Yefry-yeff@users.noreply.github.com>
2021-03-23 15:32:34 -0600

    Consulta

--- SISTEMA/HUMANSYS/app/Http/Controllers/Apicontrollers.php
+++ SISTEMA/HUMANSYS/app/Http/Controllers/Apicontrollers.php
@@ -7,43 +7,45 @@ use Illuminate\Support\Facades\DB;
 
 class Apicontrollers extends Controller
 {
-    
+
     public function gerente(){
 
         $gerente = DB::select("SELECT A.identidad, A.nombre, A.rtn, A.id
-        FROM empleado A 
-        INNER JOIN cargo B 
+        FROM empleado A
+        INNER JOIN cargo B
         ON(A.id=B.empleado_id)
-        INNER JOIN area C 
+        INNER JOIN area C
         ON(C.id=B.area_id)
         WHERE C.id=35");
 
         return $gerente;
-        
+
     }
 
 
     public function empleado_contrato(){
         $empleado = DB::select("SELECT A.id, A.identidad, A.nombre, D.nombre departamento, B.nombre cargo
         from empleado A
-        INNER JOIN cargo B 
+        INNER JOIN cargo B
         ON(A.id=B.empleado_id)
-        INNER JOIN area C 
+        INNER JOIN area C
         ON(B.area_id=C.id)
-        INNER JOIN departamento D 
+        INNER JOIN departamento D
         ON(C.departamento_id=D.id)
         WHERE A.estatus_id = 1");
-
         return $empleado;
-        
+
+       /*  $empleado =DB::select("select * from empleado;");
+        return $empleado;
+ */
     }
 
     public function area($id){
         $area = DB::select("SELECT A.id, A.nombre
-        FROM area A 
+        FROM area A
         WHERE A.departamento_id='$id'");
 
         return $area;
-        
+
     }
 }
