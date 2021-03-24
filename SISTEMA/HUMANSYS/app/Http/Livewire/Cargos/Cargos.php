82f086d1b68f15e74904589e6bf8c2ac2c8af74c
ariel112 <selvinmorazan@gmail.com>
2021-03-24 14:57:02 -0600

    cargos

--- SISTEMA/HUMANSYS/app/Http/Livewire/Cargos/Cargos.php
+++ SISTEMA/HUMANSYS/app/Http/Livewire/Cargos/Cargos.php
@@ -36,7 +36,7 @@ class Cargos extends Component
 
      
 
-        return 'EXITO';
+        return response()->json('EXITO');
 
 
     }
@@ -68,8 +68,8 @@ class Cargos extends Component
        return '<div class="dropdown dropdown-action text-right">
                 <a href="#" class="action-icon dropdown-toggle" data-toggle="dropdown" aria-expanded="false"><i class="material-icons">more_vert</i></a>
                 <div class="dropdown-menu dropdown-menu-right">
-                    <a class="dropdown-item" data-toggle="modal" data-target="#editar_cargos" ><i class="fa fa-pencil m-r-5"></i> Editar</a>
-                    <a class="dropdown-item" data-toggle="modal" data-target="#vw_cargos" ><i class="fa fa-eye m-r-5"></i> Ver</a>
+                    <a class="dropdown-item" data-toggle="modal" data-target="#editar_cargos" onclick="editcargo('.$cargos->id.')"  ><i class="fa fa-pencil m-r-5"></i> Editar</a>
+                    <a class="dropdown-item" data-toggle="modal" data-target="#vw_cargos" onclick="setcargo('.$cargos->id.')" ><i class="fa fa-eye m-r-5"></i> Ver</a>
                 </div>
             </div>';
 
@@ -89,5 +89,35 @@ class Cargos extends Component
 
     public function cargo_muestra($id){
 
+    $cargo = DB::select("SELECT A.id id_cargo,
+                                    A.nombre cargo, 
+                                    B.id id_area, B.nombre area, 
+                                    C.id id_gerencia, C.nombre gerencia,
+                                    D.nombre tipo_empleado, D.id id_empleado_tipo
+
+                                            FROM cargo A 
+                                            INNER JOIN area B 
+                                            ON(A.area_id=B.id)
+                                            INNER JOIN departamento C 
+                                            ON(B.departamento_id=C.id)
+                                            INNER JOIN tipo_empleado D
+                                            ON(D.id=A.tipo_empleado_id)
+                                            WHERE A.id='$id'");
+
+      $funciones = DB::select("SELECT * FROM funciones A
+                                WHERE A.cargo_id='$id'");
+
+      $tipo_empleado = DB::select("SELECT * FROM tipo_empleado");
+      
+
+    
+            return response()->json([                    
+                'cargo' => $cargo,
+                'funciones' => $funciones,
+                'tipo_empleado' => $tipo_empleado
+            ]);
+
     }
+
+
 }
