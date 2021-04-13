fb6ab756034cd4080d17eca15964d88858a540b7
Yefry-yeff <55094597+Yefry-yeff@users.noreply.github.com>
2021-04-13 15:21:01 -0600

    pdf

--- SISTEMA/HUMANSYS/resources/views/pdf/contrato.blade.php
+++ SISTEMA/HUMANSYS/resources/views/pdf/contrato.blade.php
@@ -4,11 +4,35 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
     <title>Contrato</title>
 </head>
-<body>
-    <h1>Contrato de {{ $contrato->nombre }}</h1>
-    <h1>sjsjsjsjsjs</h1>
-    <img   src="{{ asset('img/1.png')}}" class="imagenperfil"  alt="Alan Smith" />
+<body>{{-- Contrato de {{ $contrato->nombre }} --}}
+    <div class="container" style="position: absolute; align:center; padding:40px">
+        <br>
+        <br>
+        <br>
+        <br>
+
+        <h5 style="text-align: center;"><u>CONTRATO INDIVIDUAL DE TRABAJO POR TIEMPO DEFINIDO</u></h5>
+        <h5 style="text-align: center;">169-2021</h5>
+        <p style="text-align: justify;">Nosotros, <b>HAZEL ALEJANDRA ESCOBAR RAMIREZ</b> mayor de edad, hondureña, casada, Licenciada en Administración de Empresas, con identidad número 0801-1982-10044, Registro Tributario Nacional número 08011982100440 y de este domicilio, quien actúa en su condición de Coordinador Técnico del Programa Presidencial de Becas “Honduras 20/20” creado mediante Decreto Ejecutivo Numero PCM-013-2017, facultades que acredito mediante Acuerdo de Delegación No. GL-PPBH-001/2021 de fecha 4 de enero del presente año, quien en lo sucesivo se denominará <b>“EL CONTRATANTE” SELVIN ARIEL MORAZAN COLINDRES</b> , hondureño, mayor de edad, soltero, Ingeniero en Sistemas con tarjeta de identidad No. 0705-1994-00130 y de este domicilio, y en los sucesivo se le denominará “EL TRABAJADOR”, hemos convenido celebrar el presente <b>CONTRATO INDIVIDUAL DE TRABAJO POR TIEMPO DEFINIDO</b> el cual se regirá por las cláusulas y condiciones siguientes:</p>
+
+        <p style="text-align: justify;"><b>PRIMERO: DE LA EXCLUSIVIDAD. EL TRABAJADOR</b> manifiesta: que en este acto y por medio del presente contrato, <b>SE OBLIGA</b> a prestar sus servicios a favor del <b>CONTRATANTE</b>, comprometiendo su esfuerzo y capacidad de trabajo, en forma exclusiva, bajo la subordinación y dependencia de la <b>GERENCIA DE SEGUIMIENTO Y LOS DEMAS ADMINISTRADORES Y REPRESENTANTES QUE DESIGNE EL CONTRATANTE</b>, desempeñándose en el puesto de: <b>OFICIAL DE INFORMATICA</b>, quien llevara a cabo las siguientes funciones: 1. Responsable de diseñar y elaborar para sistemas no convencionales como ser aplicadas en celulares, según requiera la Gerencia de Seguimiento 2. Encargado de adaptar los módulos del sistema a la gerencia de seguimiento a cualquier cambio que se solicite para la mejora de este 3. Responsable de sistematizar procedimientos para reducir tiempos en cuanto a la elaboración de reportes, como ser: Reporte de horas anuales, reporte de horas trimestrales, reporte de horas anuales, y reporte de participación de los becarios por actividad 4. Responsable de generar reportes estadísticos de información en el área de seguimiento 5. Encargado de analizar la información obtenida del sistema del módulo de seguimiento con el fin de proyectar, organizar y administrar sus recursos y mejora la toma de decisiones 6. Responsable de dar soporte técnico y emplear técnicas para el apoyo a los usuarios del sistema 7. Analizar, diseñar, desarrollar y administrar módulos informáticos web Y 8. Otras actividades que se deriven de la naturaleza de su puesto de trabajo y/o de las actividades generales que realice el Programa Presidencial de Becas Honduras 20/20.</p>
+        <H1 style="PAGE-BREAK-AFTER: always"></H1>
+
+        <br><br><br>
+        <p style="text-align: justify;"><b>SEGUNDO: PERIODO DE PRUEBA. EL TRABAJADOR</b> queda sujeto a un periodo de prueba de sesenta días (60) el cual tiene por objeto apreciar por parte del CONTRATANTE las aptitudes y capacidades de <b>EL TRABAJADOR</b> y por parte de este último, si le convienen o no las condiciones de trabajo que ofrece <b>EL CONTRATANTE.</b> El periodo de prueba será remunerado.</p>
+        <p style="text-align: justify;"><b>TERCERO: VIGENCIA DEL CONTRATO.</b> El presente contrato Individual de Trabajo será por tiempo definido, con vigencia del 06 de Abril del año 2021 hasta el 30 de Junio del año 2021.</p>
+        <p style="text-align: justify;"><b>CUARTO: LUGAR DE TRABAJO.</b> EL TRABAJADOR, desempeñara las funciones y actividades en la Ciudad de Tegucigalpa M.D.C., o cualquiera de las oficinas del <b>PROGRAMA PRESIDENCIAL DE BECAS HONDURAS 20/20</b> a nivel nacional, cuando así lo requiera la naturaleza de las actividades a prestar de parte de <b>EL TRABAJADOR.</b></p>
+        <p style="text-align: justify;"><b>QUNTO: JORNADA-HORARIO DE TRABAJO.</b> La jornada de trabajo que prestará <b>EL TRABAJADOR</b>, será de lunes a jueves de 8:00 AM a 5:00 PM y los viernes de 8:00 AM a 12:00 AM, <b>EL CONTRATANTE</b> se reserva el derecho de poder cambiar este horario durante la vigencia del presente contrato siempre apegado a la jornada ordinaria ya establecida en el Código de Trabajo de Honduras. Las horas que desempeñe <b>EL TRABAJADOR</b> fuera de esta jornada de trabajo ordinaria, se consideraran horas extras siempre y cuando sean requeridas por su jefe inmediato y aprobadas por la Gerencia de Talento Humano para su pago. </p>
+        <p style="text-align: justify;"><b>SEXTO: DIA DE VACACIONES. EL TRABAJADOR</b> gozará de un día de vacaciones remuneradas por cada mes de trabajo, la solicitud de vacaciones <b>EL TRABAJADOR</b> deberá realizarla ante su jefe inmediato con 5 días de anticipación, una vez aprobado por su jefe inmediato será remitido a la Gerencia de Talento Humano para su debido control.  </p>
+        <p style="text-align: justify;">Por cada cinco (5) días de trabajo comprendidos de lunes a viernes, habrá dos (2) días de descanso, si por necesidades urgentes del <b>CONTRATANTE</b> se trabaja en días de descanso o días feriados, <b>EL TRABAJADOR</b> requerido conservara su derecho y se le será concedido cuando este lo solicite por escrito a la Gerencia de Talento Humano previo autorización de su jefe inmediato. </p>
+        <p style="text-align: justify;"><b>SEPTIMO: CONFIDENCIALIDAD DE LA INFORMACIÓN.</b> Se compromete a mantener absoluta confidencialidad y abstenerse de comunicar a terceros, directa o indirectamente, cualquier información o conocimiento de carácter financiero, técnico, contable, comercial, gerencial, administrativo o de cualquier otra naturaleza, tales como sistemas de Internet, alianzas estratégicas y demás convenios con terceros que <b>EL TRABAJADOR</b> pueda obtener o conocer durante y con ocasión de la prestación de sus servicios con <b>EL CONTRATANTE.</b> Asimismo, en la fecha de terminación de sus servicios, <b>EL TRABAJADOR</b> deberá devolver <b>AL CONTRATANTE</b>, todos los documentos, manuales, libros, correspondencia, publicaciones, herramientas y demás activos y literatura que éste pueda haber obtenido, conocido, preparado o utilizado en su trabajo mientras prestaba sus servicios para EL CONTRATANTE.</p>
+        <H1 style="PAGE-BREAK-AFTER: always"></H1>
+        <br><br><br>
+        <p style="text-align: justify;"></p>
+
+    </div>
 </body>
 </html>
