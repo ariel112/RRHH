84317bf96c096af023db1667ba7eb4a335667f5a
Yefry-yeff <55094597+Yefry-yeff@users.noreply.github.com>
2021-05-18 13:58:36 -0600

    inicios de usuarios

---
+++ SISTEMA/HUMANSYS/resources/views/livewire/user/users.blade.php
@@ -0,0 +1,17 @@
+<div class="page-wrapper">
+    <div class="content container-fluid">
+        <!-- Page Header -->
+        <div class="page-header">
+            <div class="row">
+                <div class="col-sm-12">
+                    <h3 class="page-title">Usuarios</h3>
+                    <ul class="breadcrumb">
+                        <li class="breadcrumb-item"><a href="index.html">Dashboard</a></li>
+                        <li class="breadcrumb-item active">usuarios</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+        <!-- /Page Header -->
+    </div>
+</div>
