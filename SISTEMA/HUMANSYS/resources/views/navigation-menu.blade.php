0562a410861900cfeeefb7870f4d312401c0a75d
Yefry-yeff <55094597+Yefry-yeff@users.noreply.github.com>
2021-05-31 12:02:08 -0600

    rol de usuario de jefe RRHH

--- SISTEMA/HUMANSYS/resources/views/navigation-menu.blade.php
+++ SISTEMA/HUMANSYS/resources/views/navigation-menu.blade.php
@@ -32,90 +32,6 @@
 
 
 
-					<!-- Notifications -->
-					{{-- <li class="nav-item dropdown">
-						<a href="#" class="dropdown-toggle nav-link" data-toggle="dropdown">
-							<i class="fa fa-bell-o"></i> <span class="badge badge-pill">3</span>
-						</a>
-						<div class="dropdown-menu notifications">
-							<div class="topnav-dropdown-header">
-								<span class="notification-title">Notifications</span>
-								<a href="javascript:void(0)" class="clear-noti"> Clear All </a>
-							</div>
-							<div class="noti-content">
-								<ul class="notification-list">
-									<li class="notification-message">
-										<a href="activities.html">
-											<div class="media">
-												<span class="avatar">
-												</span>
-												<div class="media-body">
-													<p class="noti-details"><span class="noti-title">John Doe</span> added new task <span class="noti-title">Patient appointment booking</span></p>
-													<p class="noti-time"><span class="notification-time">4 mins ago</span></p>
-												</div>
-											</div>
-										</a>
-									</li>
-									<li class="notification-message">
-										<a href="activities.html">
-											<div class="media">
-												<span class="avatar">
-												</span>
-												<div class="media-body">
-													<p class="noti-details"><span class="noti-title">Tarah Shropshire</span> changed the task name <span class="noti-title">Appointment booking with payment gateway</span></p>
-													<p class="noti-time"><span class="notification-time">6 mins ago</span></p>
-												</div>
-											</div>
-										</a>
-									</li>
-									<li class="notification-message">
-										<a href="activities.html">
-											<div class="media">
-												<span class="avatar">
-
-												</span>
-												<div class="media-body">
-													<p class="noti-details"><span class="noti-title">Misty Tison</span> added <span class="noti-title">Domenic Houston</span> and <span class="noti-title">Claire Mapes</span> to project <span class="noti-title">Doctor available module</span></p>
-													<p class="noti-time"><span class="notification-time">8 mins ago</span></p>
-												</div>
-											</div>
-										</a>
-									</li>
-									<li class="notification-message">
-										<a href="activities.html">
-											<div class="media">
-												<span class="avatar">
-													<img alt="" src="assets/img/profiles/avatar-17.jpg">
-												</span>
-												<div class="media-body">
-													<p class="noti-details"><span class="noti-title">Rolland Webber</span> completed task <span class="noti-title">Patient and Doctor video conferencing</span></p>
-													<p class="noti-time"><span class="notification-time">12 mins ago</span></p>
-												</div>
-											</div>
-										</a>
-									</li>
-									<li class="notification-message">
-										<a href="activities.html">
-											<div class="media">
-												<span class="avatar">
-													<img alt="" src="assets/img/profiles/avatar-13.jpg">
-												</span>
-												<div class="media-body">
-													<p class="noti-details"><span class="noti-title">Bernardo Galaviz</span> added new task <span class="noti-title">Private chat module</span></p>
-													<p class="noti-time"><span class="notification-time">2 days ago</span></p>
-												</div>
-											</div>
-										</a>
-									</li>
-								</ul>
-							</div>
-							<div class="topnav-dropdown-footer">
-								<a href="activities.html">View all Notifications</a>
-							</div>
-						</div>
-					</li> --}}
-					<!-- /Notifications -->
-
 
 					<!-- /Message Notifications -->
 
@@ -234,7 +150,7 @@
 								<span>Menu</span>
 							</li>
 
-                            @if (Auth::user()->id_tipo_user == 1)
+                            @if (Auth::user()->id_tipo_user == 1|| Auth::user()->id_tipo_user == 3)
                                 <li class="submenu">
                                     <a href="#" ><i class="la la-user"></i> <span>Usuarios</span> <span class="menu-arrow"></span></a>
                                     <ul style="display: none;">
